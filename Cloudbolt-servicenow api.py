e38ff3b3e23a4ded6439462f302369d995778207
b2c-team <b2c-algemeen@cmp-select.org>
2018-07-23 14:15:24 +0200

    Update Cloudbolt-servicenow api.py

--- Cloudbolt-servicenow api.py
+++ Cloudbolt-servicenow api.py
@@ -17,10 +17,8 @@ def run(job, logger=None):
     if job.status == 'FAILURE':
         return "", "", ""
     
-    conn = ''
-
-    """conn = ConnectionInfo.objects.get(name='connect
-    assert isinstance(conn, ConnectionInfo)"""
+    conn = ConnectionInfo.objects.get(name='connect'.format(server.environment.id)).first())
+    assert isinstance(conn, ConnectionInfo)
 
     servicenow_url = "{https}://{dev51482.service-now.com/}:{443}".format(conn.protocol, conn.ip, conn.port)
 
